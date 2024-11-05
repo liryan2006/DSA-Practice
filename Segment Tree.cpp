c4a33f04942842ef90fdaf36bcb123001070bea1
liryan2006 <ryanliwendi@gmail.com>
2024-11-05 07:45:22 -0800

    Add Lazy

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -26,6 +26,7 @@ using namespace std;
 const int LEN = 6;
 vector<int> nums(LEN);
 vector<int> ST(4 * LEN, 0); // Typically initialize 4 times the length of the array
+vector<int> lazy(4 * LEN, 0);
 
 void generateSegTree(int start, int end, int pos)
 {
@@ -42,7 +43,7 @@ void generateSegTree(int start, int end, int pos)
     ST[pos] = ST[2 * pos] + ST[2 * pos + 1];
 }
 
-void update(int pos, int idx, int val, int start, int end)
+void updateValue(int pos, int idx, int val, int start, int end)
 {
     if(start == end)
     {
@@ -53,15 +54,50 @@ void update(int pos, int idx, int val, int start, int end)
 
     int mid = (start + end) / 2;
     if(idx <= mid)
-        update(2 * pos, idx, val, start, mid);
+        updateValue(2 * pos, idx, val, start, mid);
     else
-        update(2 * pos + 1, idx, val, mid + 1, end);
+        updateValue(2 * pos + 1, idx, val, mid + 1, end);
 
     ST[pos] = ST[2 * pos] + ST[2 * pos + 1];
 }
 
+void update(int pos, int start, int end)
+{
+    ST[pos] += (end - start + 1) * lazy[pos];
+    if(start != end)
+    {
+        lazy[2 * pos] += lazy[pos];
+        lazy[2 * pos + 1] += lazy[pos];
+    }
+    lazy[pos] = 0;
+}
+
+void updateRange(int pos, int qstart, int qend, int start, int end, int val)
+{
+    if(qstart == start && qend == end)
+    {
+        lazy[pos] += val;
+        return;
+    }
+
+//    if(lazy[pos]) update(pos, start, end);
+    int mid = (start + end) / 2;
+    if(qend <= mid)
+        updateRange(2 * pos, qstart, qend, start, mid, val);
+    else if(qstart > mid)
+        updateRange(2 * pos + 1, qstart, qend, mid + 1, end, val);
+    else
+    {
+        updateRange(2 * pos, qstart, mid, start, mid, val);
+        updateRange(2 * pos + 1, mid + 1, qend, mid + 1, end, val);
+    }
+    ST[pos] = ST[2 * pos] + ST[2 * pos + 1]
+            + (mid - start + 1) * lazy[2 * pos] + (end - mid) * lazy[2 * pos + 1];
+}
+
 int query(int pos, int qstart, int qend, int start, int end)
 {
+    if(lazy[pos]) update(pos, start, end);
     if(qstart == start && qend == end)
         return ST[pos];
 
@@ -89,15 +125,22 @@ int main()
     cout << endl;
 
     generateSegTree(0, LEN - 1, 1);
-    update(1, 0, 10, 0, LEN - 1); // Add num[0] by 10
-    update(1, 5, 10, 0, LEN - 1); // Add num[5] by 10
+    updateValue(1, 0, 10, 0, LEN - 1); // Add num[0] by 10
+    updateValue(1, 5, 10, 0, LEN - 1); // Add num[5] by 10
 
     cout << "The array after update is: ";
     for(auto num : nums)
         cout << num << " ";
     cout << endl;
 
-    cout << "The sum from num[2] to num[5] is: " << query(1,2,5,0,5);
+    cout << "The sum from num[2] to num[5] is: ";
+    cout << query(1,2,5,0,5) << endl;
+
+    updateRange(1, 1, 3, 0, LEN - 1, 5);
+    cout << query(1, 0, 3, 0, LEN - 1) << endl;
+    cout << query(1, 1, 2, 0, LEN - 1) << endl;
+
+    return 0;
 }
 
 
